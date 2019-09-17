79f23300a5f1ef37bb5188b5856c06368fa01693
ms <margarita.stepina@gmail.com>
2019-09-16 22:31:34 -0700

    ex02

---
+++ day05/ex02/ex02.sql
@@ -0,0 +1,6 @@
+insert into ft_table (login, `group`, `creation_date`) values 
+('loki', 'staff', '2013-05-01'),
+('scadoux', 'student', '2014-01-01'),
+('chap', 'student', '2014-01-01'),
+('bambou', 'staff', '2014-03-01'),
+('fantomet', 'staff', '2010-04-03');
